7d99fece5172d44b8cd9b7ed2ae30c9a74d32146
Luis Torres <luisdtor@gmail.com>
2021-10-19 13:14:51 -0500

    Setup routing and hooked up login inputs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import LoginView from './LoginView/LoginView';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginView from "./LoginView/LoginView";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <LoginView />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <LoginView />
+          </Route>
+          <Route path="/menu">
+            <h1>Main Menu</h1>
+          </Route>
+          <Route>
+            <h1>Not found</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/LoginView/LoginForm.js
+++ src/LoginView/LoginForm.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginForm.css";
 
 function LoginForm() {
+  const initialFormData = {
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormData });
+
+  const handleInputChange = (e) => {
+    setFormData((currentData) => {
+      return {
+        ...currentData,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+
+    setFormData({ ...initialFormData });
+  };
+
   return (
     <div className="login-form-container">
       <h3 className="login-form-heading">Login</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={handleInputChange}
+            value={formData.username}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="text" id="password" />
+          <input
+            type="text"
+            id="password"
+            name="password"
+            onChange={handleInputChange}
+            value={formData.password}
+          />
         </div>
         <button className="login-submit-button" type="submit">
           Log In

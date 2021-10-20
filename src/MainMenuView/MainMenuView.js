41c3121e020d75bcfee7e36c57c7080d60c37d34
Luis Torres <luisdtor@gmail.com>
2021-10-20 14:38:26 -0500

    Added markup for main menu view

--- src/MainMenuView/MainMenuView.js
+++ src/MainMenuView/MainMenuView.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import NavBar from "../Common/NavBar";
+import customerService from "../images/customer-service.png";
+import repairs from "../images/repairs.png";
 import "./MainMenuView.css";
 
 function MainMenuView() {
@@ -7,6 +10,20 @@ function MainMenuView() {
     <div>
       <NavBar />
       <h1>Main Menu</h1>
+      <main>
+        <Link to="">
+          <figure>
+            <img src={customerService} alt="customer service" />
+            <figcaption>Customer Service</figcaption>
+          </figure>
+        </Link>
+        <Link to="">
+          <figure>
+            <img src={repairs} alt="repairs" />
+            <figcaption>Repairs</figcaption>
+          </figure>
+        </Link>
+      </main>
     </div>
   );
 }

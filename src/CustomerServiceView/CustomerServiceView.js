3078f04be70144dc2342fce1e68a3e26e2a0acc0
Luis Torres <luisdtor@gmail.com>
2021-10-20 19:07:35 -0500

    Added markup for customer service view

--- src/CustomerServiceView/CustomerServiceView.js
+++ src/CustomerServiceView/CustomerServiceView.js
@@ -5,8 +5,26 @@ import NavBar from "../Common/NavBar";
 
 function CustomerServiceView() {
   return (
-    <div>
+    <div className="customer-service-view">
       <NavBar />
+      <h1>Customer Service Training</h1>
+      <main>
+        <div className="video-link-container">
+          <a href="https://www.youtube.com/watch?v=tlNK3eJvsz0" target="_blank" rel="noreferrer">
+            Module 1
+          </a>
+        </div>
+        <div className="video-link-container">
+          <a href="https://www.youtube.com/watch?v=kx7-S9jvVXM" target="_blank" rel="noreferrer">
+            Module 2
+          </a>
+        </div>
+        <div className="video-link-container">
+          <a href="https://www.youtube.com/watch?v=tb2rv77YO-w" target="_blank" rel="noreferrer">
+            Module 3
+          </a>
+        </div>
+      </main>
     </div>
   );
 }

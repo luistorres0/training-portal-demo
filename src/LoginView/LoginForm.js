56b2a84e19d0dc8d741944aba08976db61e96bc0
Luis Torres <luisdtor@gmail.com>
2021-10-21 14:31:03 -0500

    Added validation for login form

--- src/LoginView/LoginForm.js
+++ src/LoginView/LoginForm.js
@@ -10,6 +10,8 @@ function LoginForm({ setIsLoggedIn }) {
 
   const history = useHistory();
   const [formData, setFormData] = useState({ ...initialFormData });
+  const [usernameError, setUsernameError] = useState("none");
+  const [passwordError, setPasswordError] = useState("none");
 
   const handleInputChange = (e) => {
     setFormData((currentData) => {
@@ -20,36 +22,58 @@ function LoginForm({ setIsLoggedIn }) {
     });
   };
 
+  const validateFormData = () => {
+    const { username, password } = formData;
+    setUsernameError("none");
+    setPasswordError("none");
+
+    let isValid = true;
+    if (username.length === 0) {
+      setUsernameError("Username cannot be empty");
+      isValid = false;
+    }
+    
+    if (password.length === 0) {
+      setPasswordError("Password cannot be empty");
+      isValid = false;
+    } else if (password.length < 7) {
+      setPasswordError("Password must have at least 7 characters");
+      isValid = false;
+    } else if (!password.includes("#")) {
+      setPasswordError("Password must contain a '#' symbol");
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:5000/users/login/", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ data: formData }),
-      });
-
-      const data = await response.json();
-
-      if (data.message === "authenticated") {
-        setIsLoggedIn(true);
-        history.push("/menu");
-      }
-
-      // TODO: When creating a new user, use the code below.
-      // await fetch("http://localhost:5000/users/", {
-      //   method: "post",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ data: formData }),
-      // });
-    } catch (error) {
-      console.log(error);
+    if (validateFormData()) {
+      console.log("valid");
+    } else {
+      console.log("not valid");
     }
+
+    // try {
+    //   const response = await fetch("http://localhost:5000/users/login/", {
+    //     method: "post",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ data: formData }),
+    //   });
+
+    //   const data = await response.json();
+
+    //   if (data.message === "authenticated") {
+    //     setIsLoggedIn(true);
+    //     history.push("/menu");
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
 
   return (
@@ -65,6 +89,7 @@ function LoginForm({ setIsLoggedIn }) {
             onChange={handleInputChange}
             value={formData.username}
           />
+          <small className={usernameError !== "none" ? "show" : ""}>{usernameError}</small>
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
@@ -75,6 +100,7 @@ function LoginForm({ setIsLoggedIn }) {
             onChange={handleInputChange}
             value={formData.password}
           />
+          <small className={passwordError !== "none" ? "show" : ""}>{passwordError}</small>
         </div>
         <button className="login-submit-button" type="submit">
           Log In
